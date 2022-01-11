de6fe3e284076c897a46da6a2b826f0165a1335b
Protik111 <rafiurprotik111@gmail.com>
2022-01-11 14:18:38 +0600

    client navbar added

---
+++ client/src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import log from '../../../images/blogs.png';
+import styles from './Navbar.module.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
+        <nav className={styles.navbar_container}>
+            <div className={styles.logo_container}>
+                <img src={log} alt="" />
+            </div>
+            <div className={styles.navbar_links}>
+                <p className={styles.navbar_links_item}><a href="/#home">Home</a></p>
+                <p className={styles.navbar_links_item}><a href="/#blogs">Blogs</a></p>
+                <p className={styles.navbar_links_item}><a href="/#about">About</a></p>
+                <p className={styles.navbar_links_item}><a href="/#login">Login</a></p>
+            </div>
+
+            <div className={styles.navbar_links_menu}>
+                {
+                    toggleMenu ?
+                        <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+                        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+                }
+                {
+                    toggleMenu && (<div className={styles.navbar_links_menu_items}>
+                        <p className={styles.navbar_links_item}><a href="">Home</a></p>
+                        <p className={styles.navbar_links_item}><a href="">Blogs</a></p>
+                        <p className={styles.navbar_links_item}><a href="">About</a></p>
+                        <p className={styles.navbar_links_item}><a href="">Login</a></p>
+                    </div>)
+                }
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
