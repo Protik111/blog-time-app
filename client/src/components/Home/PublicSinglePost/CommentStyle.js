1d83f0c88f1bfec234dacc0492745ed0e83cf3af
Protik111 <rafiurprotik111@gmail.com>
2022-02-01 14:10:21 +0600

    delete button conditionaly show

--- client/src/components/Home/PublicSinglePost/CommentStyle.js
+++ client/src/components/Home/PublicSinglePost/CommentStyle.js
@@ -3,8 +3,9 @@ import styles from './Comment.module.css';
 import { deleteComment } from '../../../redux/action/Post.action';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAlert } from '../../../redux/action/Alert.action';
-import Alert from '../../Alert/Alert';
-import { laodCommentor } from '../../../redux/action/User.action';
+import Moment from 'react-moment';
+import blankPhoto from '../../../images/slides/blank.png';
+
 
 const CommentStyle = ({ comment, id }) => {
     const { user, isAuthenticated } = useSelector((state => state.authReducer));
@@ -12,19 +13,33 @@ const CommentStyle = ({ comment, id }) => {
     const handleCommentDelete = () => {
         if (isAuthenticated) {
             dispatch(deleteComment(id, comment._id));
-        }else{
+        } else {
             dispatch(setAlert('Must Be Login To Delete', 'notCreated'))
         }
     }
-    console.log('commentstyler', comment.user);
+    console.log('commentstyler', user, comment.user);
+
+    // let buttonShow = null;
+    // if (user === null) {
+    //     buttonShow = false;
+    //     user._id = null;
+    // }else{
+    //     buttonShow = user._id === comment.user
+    // }
+
     return (
         <div className="d-flex">
-            <div className={`${styles.comment_container} p-3 w-75 m-2 d-flex justify-content-start`}>
-                <p>{comment.name}--</p>
-                <h6>{comment.text}</h6>
+            <div className={`${styles.comment_container} p-3 w-75 m-2 d-flex justify-content-between`}>
+                <div className="">
+                    <p>{comment.text}</p>
+                </div>
+                <div className={`${styles.author}`}>
+                    <p>By {comment.name}</p>
+                    <p>Posted<Moment format="D MMM YYYY" withTitle>{comment.date}</Moment></p>
+                </div>
             </div>
             <div>
-                <button onClick={handleCommentDelete} className="btn btn-danger m-4">Delete</button>
+                {user !== null ? user._id === comment.user ? <button onClick={handleCommentDelete} className="btn btn-danger m-4">Delete</button> : '' : ''}
             </div>
         </div>
     );
